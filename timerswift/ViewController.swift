9ec310efb31a64414c5256630bd05678446323cf
David Lang <David@Davids-iMac.local>
2015-01-26 17:33:33 -0600

    Initial Commit

---
+++ timerswift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  timerswift
+//
+//  Created by David Lang on 1/26/15.
+//  Copyright (c) 2015 David Lang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
